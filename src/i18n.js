e471f865bc775c8d41b4c3c8fc002401de4393c1
Tim Sweet <timothy.sweet@jorsek.com>
2020-12-15 16:42:46 -0800

    enhancing I18n support to allow for server-side translations and better handling of language strings

--- src/i18n.js
+++ src/i18n.js
@@ -2,101 +2,117 @@ const DataManager = require("./data-manager");
 const { template } = require("./utils");
 const withContext = require("./context-binding");
 
-if (typeof HTMLElement !== "undefined") {
-  /**
-   * @class I18nMessage
-   * @description <i18n-message> HTML element. Provides tranlsation and interpolation for
-   * translatable strings
-   * @param {String} key the key for the strings based on current language. can be set as the innerHTML or
-   * defined as the attibutes: key, id, data-key, data-id
-   * @param {JSON} values can be passed as data-* attributes or as a json-parseable object string as "data-values"
-   * @param {String} dataAttributes
-   * @example <caption>Given the following configuration</caption>
-   * import { I18n } from '@ornery/web-components';
-   * I18n.addMessages('en-US', {
-   *  'translatable.message.name': "I'm a translated string from i18n",
-   *   'tokenized.message': "I have a ${color} ${animal}"
-   * });
-   * @example @lang html <caption>With the following usage</caption>
-   * <i18n-message>translatable.message.name</i18n-message>
-   * <div>
-   *    <i18n-message data-values="{'color: 'grey', 'animal': 'monkey'}">tokenized.message</i18n-message>
-   *    <i18n-message data-color="grey" data-animal="monkey">tokenized.message</i18n-message>
-   *    <i18n-message key="tokenized.message"/>
-   *    <!-- React does not pass key or ref props so you can use "data-key" or "data-id" as well-->
-   *    <i18n-message data-key="tokenized.message"/>
-   *    <i18n-message id="translatable.message.name"/>
-   *    <i18n-message data-id="translatable.message.name"/>
-   * </div>
-   *
-   * @example @lang html <caption>Renders the HTML</caption>
-   * <i18n-message>I'm a translated string from i18n</i18n-message>
-   * <i18n-message>I have a grey monkey</i18n-message>
-   */
-  class I18nMessage extends HTMLElement {
+if (typeof HTMLElement === "undefined") {
+    HTMLElement = class {};
+}
+/**
+ * @class I18nMessage
+ * @description <i18n-message> HTML element. Provides tranlsation and interpolation for
+ * translatable strings
+ * @param {String} key the key for the strings based on current language. can be set as the innerHTML or
+ * defined as the attibutes: key, id, data-key, data-id
+ * @param {JSON} values can be passed as data-* attributes or as a json-parseable object string as "data-values"
+ * @param {String} dataAttributes
+ * @example <caption>Given the following configuration</caption>
+ * import { I18n } from '@ornery/web-components';
+ * I18n.addMessages('en-US', {
+ *  'translatable.message.name': "I'm a translated string from i18n",
+ *   'tokenized.message': "I have a ${color} ${animal}"
+ * });
+ * @example @lang html <caption>With the following usage</caption>
+ * <i18n-message>translatable.message.name</i18n-message>
+ * <div>
+ *    <i18n-message data-values="{'color: 'grey', 'animal': 'monkey'}">tokenized.message</i18n-message>
+ *    <i18n-message data-color="grey" data-animal="monkey">tokenized.message</i18n-message>
+ *    <i18n-message key="tokenized.message"/>
+ *    <!-- React does not pass key or ref props so you can use "data-key" or "data-id" as well-->
+ *    <i18n-message data-key="tokenized.message"/>
+ *    <i18n-message id="translatable.message.name"/>
+ *    <i18n-message data-id="translatable.message.name"/>
+ * </div>
+ *
+ * @example @lang html <caption>Renders the HTML</caption>
+ * <i18n-message>I'm a translated string from i18n</i18n-message>
+ * <i18n-message>I have a grey monkey</i18n-message>
+ */
+class I18nMessage extends HTMLElement {
     constructor(self) {
-      self = super(self);
-      this.attachShadow({ mode: "open" });
-      return self;
+        self = super(self);
+        this.attachShadow({ mode: "open" });
+        return self;
     }
 
     get translate() {
-      return (
-        this.innerHTML || this.getAttribute("key") || this.getAttribute("id")
-      );
+        return (
+            this.innerHTML || this.getAttribute("key") || this.getAttribute("id")
+        );
     }
 
     update() {
-      const context = { ...this.getAttribute("data-values"), ...this.dataset };
-      this.shadowRoot.innerHTML = I18n.get(this.translate, context);
+        const context = { ...this.getAttribute("data-values"), ...this.dataset };
+        this.shadowRoot.innerHTML = I18n.get(this.translate, context);
     }
 
     connectedCallback() {
-      DataManager.subscribeTo("i18n-messages", () => {
-        this.update();
-      });
-      const attrObserver = new MutationObserver(() => this.update());
-      attrObserver.observe(this, { attributes: true, childList: true });
+        DataManager.subscribeTo("i18n-messages", () => {
+            this.update();
+        });
+        const attrObserver = new MutationObserver(() => this.update());
+        attrObserver.observe(this, { attributes: true, childList: true });
     }
-  }
+}
 
-  /**
-   *
-   * @class I18n
-   * @description Import strings here and call I18n.addStrings() with the supported locale identifier
-   * and the strings object exported from the language file
-   * By default, it will set the values on window.i18n, if defined when loaded, as the starting messages.
-   * This is useful if you wish to server-side render HTML certain content before laoding scripts on the client.
-   * @example
-   *
-   * import { I18n } from '@ornery/web-components';
-   *
-   * I18n.addMessages('en-US', {
-   *     'translatable.message.name': "I'm a translated string from i18n",
-   *     'tokenized.message': "I have a ${this.color} ${this.animal}"
-   * });
-   *
-   * @example
-   *
-   * window.i18n = {
-   *     'en-US': {
-   *         'translatable.message.name': "I'm a translated string from i18n",
-   *         'tokenized.message': "I have a ${this.color} ${this.animal}"
-   *     }
-   * }
-   *
-   * import { I18n } from '@ornery/web-components';
-   *
-   * console.log(I18n.getMessages()) // will log the window.i18n object
-   *
-   */
-  const I18n = new (class {
+/**
+ *
+ * @class I18n
+ * @description Import strings here and call I18n.addStrings() with the supported locale identifier
+ * and the strings object exported from the language file
+ * By default, it will set the values on window.i18n, if defined when loaded, as the starting messages.
+ * This is useful if you wish to server-side render HTML certain content before laoding scripts on the client.
+ * @example
+ *
+ * import { I18n } from '@ornery/web-components';
+ *
+ * I18n.addMessages('en-US', {
+ *     'translatable.message.name': "I'm a translated string from i18n",
+ *     'tokenized.message': "I have a ${this.color} ${this.animal}"
+ * });
+ *
+ * @example
+ *
+ * window.i18n = {
+ *     'en-US': {
+ *         'translatable.message.name': "I'm a translated string from i18n",
+ *         'tokenized.message': "I have a ${this.color} ${this.animal}"
+ *     }
+ * }
+ *
+ * import { I18n } from '@ornery/web-components';
+ *
+ * console.log(I18n.getMessages()) // will log the window.i18n object
+ *
+ */
+const I18n = new (class {
     constructor() {
-      if (typeof window !== "undefined") {
-        this._lang = navigator.language || "en-US"; // language without region code;
-        this.setMessages(window.i18n || {});
-        customElements.define("i18n-message", withContext(I18nMessage));
-      }
+        let defaultLang = "en-US";
+        if (typeof window !== "undefined") {
+            if (typeof navigator !== "undefined") {
+                defaultLang = navigator.language;
+            }
+            if (
+                typeof customElements !== "undefined" &&
+                !customElements.get("i18n-message")
+            ) {
+                customElements.define("i18n-message", withContext(I18nMessage));
+            }
+            this.setMessages(window.i18n || {});
+        } else {
+            if (typeof process !== "undefined") {
+                const {env} = process;
+                defaultLang = env.LANG || env.LANGUAGE || env.LC_ALL || env.LC_MESSAGES || defaultLang;
+            }
+        }
+        this.setLang(defaultLang); // language without region code;
     }
 
     /**
@@ -111,7 +127,7 @@ if (typeof HTMLElement !== "undefined") {
      * console.log(I18n.getLang()) // "en-US"
      */
     getLang() {
-      return this._lang;
+        return this._lang;
     }
     /**
      * @memberof I18n
@@ -125,7 +141,7 @@ if (typeof HTMLElement !== "undefined") {
      * console.log(I18n.getLang()) //'en-US'
      */
     setLang(lang) {
-      this._lang = lang.toLowerCase();
+        this._lang = lang;
     }
     /**
      * @memberof I18n
@@ -133,7 +149,7 @@ if (typeof HTMLElement !== "undefined") {
      * @description returns the current i18n messages set in the DataManager
      */
     getMessages() {
-      return DataManager.get("i18n-messages") || {};
+        return DataManager.get("i18n-messages") || {};
     }
     /**
      * @memberof I18n
@@ -152,7 +168,7 @@ if (typeof HTMLElement !== "undefined") {
      * })
      */
     setMessages(values) {
-      DataManager.set("i18n-messages", values) || {};
+        DataManager.set("i18n-messages", values) || {};
     }
     /**
      * @memberof I18n
@@ -169,12 +185,19 @@ if (typeof HTMLElement !== "undefined") {
      * });
      */
     addMessages(lang, strings) {
-      const existing = this.getMessages();
-      const newMessages = {
-        ...existing,
-        [lang]: { ...existing[lang], ...strings },
-      };
-      this.setMessages(newMessages);
+        const existing = this.getMessages();
+        const altLang = lang.split(/[_-]/i)[0];
+        const addedMessages = { 
+            ...existing[altLang], 
+            ...existing[lang], 
+            ...strings 
+        };
+        const newMessages = {
+            ...existing,
+            [altLang]: addedMessages,
+            [lang]: addedMessages
+        };
+        this.setMessages(newMessages);
     }
 
     /**
@@ -198,13 +221,13 @@ if (typeof HTMLElement !== "undefined") {
      * console.log(I18n.get('tokenized.message', stringTestData)) // "I have a grey monkey"
      */
     get(key, data) {
-      const strings = this.getMessages()[this._lang];
-      let string = strings && strings[key];
-      if (data) {
-        string = template(string, data);
-      }
-      // type coercion desired in this case for string = undefined possibility
-      return string;
+        const strings = this.getMessages()[this.getLang()];
+        let string = strings && strings[key];
+        if (data) {
+            string = template(string, data);
+        }
+        // type coercion desired in this case for string = undefined possibility
+        return string;
     }
 
     /**
@@ -222,39 +245,25 @@ if (typeof HTMLElement !== "undefined") {
      *   'tokenized.message': "I have a ${color} ${animal}"
      * });
      *
-     * console.log(I18n.getAll('tokenized')) // {"message": "I have a ${color} ${animal}"}
+     * console.log(I18n.getAll('tokenized', {color: "red", animal: "panda"})) // {"message": "I have a red panda"}
      */
-    getAll(namespace) {
-      if (namespace) {
-        return Object.entries(this.getMessages()[this._lang])
-          .filter(([key, value]) => {
-            return key.startsWith(namespace) && value;
-          })
-          .reduce((reducer, [key, val]) => {
-            reducer[key.replace(namespace + ".", "")] = val;
-            return reducer;
-          }, {});
-      } else {
-        return this.getMessages()[this._lang];
-      }
+    getAll(namespace, data) {
+        if (namespace) {
+            return Object.entries(this.getMessages()[this.getLang()])
+                .filter(([key, value]) => {
+                    return key.startsWith(namespace) && value;
+                })
+                .reduce((reducer, [key, val]) => {
+                    reducer[key.replace(namespace + ".", "")] = this.get(key, data);
+                    return reducer;
+                }, {});
+        } else {
+            return this.getMessages()[this.getLang()];
+        }
     }
-  })();
+})();
 
-  module.exports = {
+module.exports = {
     I18n,
     I18nMessage,
-  };
-} else {
-  module.exports = {
-    I18n: {
-      get: () => {},
-      getAll: () => {},
-      addMessages: () => {},
-      setMessages: () => {},
-      getMessages: () => {},
-      getLang: () => {},
-      setLang: () => {},
-    },
-    I18nMessage: class {},
-  };
-}
+};
